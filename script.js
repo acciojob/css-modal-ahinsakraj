8c32960897c77bc8af61c12e5498efa9243d99d8
Ahinsak Raj <117904029+ahinsakraj@users.noreply.github.com>
2023-12-06 22:12:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+function onOpenModal() {
+	 var modal = document.getElementById('myModal');
+     var btn = document.getElementById('openModal');
+	  modal.style.display = 'block';
+}
+
+// Function to close the modal
+function onCloseModal() {
+	var closeBtn = document.querySelector('.close-modal');
+	var modal = document.getElementById('myModal');
+	modal.style.display = 'none';
+}
+
+ window.addEventListener('click', function (event) {
+	 var modal = document.getElementById('myModal');
+	 if (event.target === modal) {
+          onCloseModal();
+        }
+});
